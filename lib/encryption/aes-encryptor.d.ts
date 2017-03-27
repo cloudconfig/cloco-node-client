6da54fee17d4a91e0c869f525aee81771fa326b9
345andrew <andrew@345.systems>
2017-03-27 18:02:37 +0100

    Adds support for salted AES

--- lib/encryption/aes-encryptor.d.ts
+++ lib/encryption/aes-encryptor.d.ts
@@ -1,22 +1,48 @@
+/// <reference types="node" />
 import { IEncryptor } from "./iencryptor";
+/**
+ * Class to hold the intermediate data settings.
+ */
+export declare class EncryptionParameters {
+    content: Buffer;
+    data: Buffer;
+    iv: Buffer;
+    key: Buffer;
+    salt: Buffer;
+}
+/**
+ * Class containing logic for encrypting and decrypting data using AES256.
+ */
 export declare class AesEncryptor implements IEncryptor {
     /**
-     * The encryption key.
+     * The encryption passphrase.
      * @type {string}
      */
-    key: string;
+    passphrase: string;
     /**
      * Initializes a new instance of the AesEncryptor class.
-     * @param  {string} encryptionKey The encryption key.
+     * @param  {string} passphrase    The encryption passphrase.
      * @return {void}                 Void.
      */
-    constructor(encryptionKey: string);
+    constructor(passphrase: string);
     /**
      * Encrypts the object by JSON serializing it and then calling AES.
      */
     encrypt(data: string): string;
     /**
-     * Decrypts the object and then parses it as JSON.
+     * Decrypts the object using AES256.
      */
     decrypt(encrypted: string): string;
+    /**
+     * Derives the key, iv and content from a salted AES packet.
+     * @param  {string}               data   The data.
+     * @param  {string}               format The format of the data.
+     * @return {EncryptionParameters}        The encryption parameters.
+     */
+    deriveKeyAndIV(data: string, format?: string): EncryptionParameters;
+    /**
+     * Creates the key and IV using the passphrase and a random salt.
+     * @return {EncryptionParameters}        The encryption parameters.
+     */
+    createKeyAndIV(): EncryptionParameters;
 }
